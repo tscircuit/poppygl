2bf4740c1bf618a22bf962d3763f8c8c58c92aee
Incharajayaram <incharajayram2020@gmail.com>
2025-10-07 23:14:41 +0530

    Fix formatting

--- tests/basics/transparent.test.ts
+++ tests/basics/transparent.test.ts
@@ -4,9 +4,7 @@ import { renderGLTFToPNGBuffer } from "../../cli/renderGLTFToPNGBuffer.ts"
 import "../fixtures/preload.ts"
 
 test("transparent materials", async () => {
-  const gltfPath = fileURLToPath(
-    new URL("./transparent.gltf", import.meta.url),
-  )
+  const gltfPath = fileURLToPath(new URL("./transparent.gltf", import.meta.url))
   const pngBuffer = await renderGLTFToPNGBuffer(gltfPath, {
     width: 640,
     height: 480,
